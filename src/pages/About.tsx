9a02155131e9cb4ac0d7142ab698204ae0b04338
person <personx92lala@gmail.com>
2025-06-10 22:33:13 +0200

    about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,10 @@
 
 
 export default function About() {
-  return <div>About Page</div>;
+  return <div>About Page
+  <h1>Something.....blablablablaaa</h1>
+    <h2>Something...........</h2>
+    <h2>Something...........</h2>
+    <h2>Something...........</h2>
+  </div>;
 }
